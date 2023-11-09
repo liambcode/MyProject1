bcfa50d2a79b6024c610c9ab01a9b54d5b898498
liambcode <101268363+liambcode@users.noreply.github.com>
2023-11-09 17:01:16 -0500

    added raft script

---
+++ My project (1)/Assets/scripts/RaftTrigger.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaftTrigger : MonoBehaviour
+{
+    public int distance;
+    public float speed;
+    private Vector3 startPos, targetPos;
+
+    private void Awake()
+    {
+        startPos = transform.position;
+        targetPos = startPos + transform.forward * distance;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+    }
+
+}
